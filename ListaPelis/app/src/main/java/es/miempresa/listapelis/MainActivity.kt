67ececfd5cc052bfd76e411b934e80a5e7b35383
JoseluLopezMesa <joselulm9@gmail.com>
2021-11-08 19:30:48 +0100

    Tarea 4

--- ListaPelis/app/src/main/java/es/miempresa/listapelis/MainActivity.kt
+++ ListaPelis/app/src/main/java/es/miempresa/listapelis/MainActivity.kt
@@ -2,10 +2,33 @@ package es.miempresa.listapelis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("joselu","onCreate")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("joselu","onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("joselu","onDestroy")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("joselu","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("joselu","onStop")
     }
 }
