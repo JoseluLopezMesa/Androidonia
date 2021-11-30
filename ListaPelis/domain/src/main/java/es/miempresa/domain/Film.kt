1e8557ea93af8b37337165956bce4ef8f1b1a842
JoseluLopezMesa <joselulm9@gmail.com>
2021-11-30 17:16:55 +0100

    Domain:   + attrs

--- ListaPelis/domain/src/main/java/es/miempresa/domain/Film.kt
+++ ListaPelis/domain/src/main/java/es/miempresa/domain/Film.kt
@@ -1,3 +1,7 @@
 package es.miempresa.domain
 
-data class Film ( val title: String )
+data class Film (
+    val title: String,
+    val urlImg:String,
+    val rating:Double,
+    val directorName:String )
