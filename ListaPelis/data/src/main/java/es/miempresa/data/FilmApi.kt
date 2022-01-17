47c72ae12190ec78830b9cdac1e1e218094a50d9
JoseluLopezMesa <joselulm9@gmail.com>
2022-01-17 19:59:13 +0100

    Actividad 9.3

--- ListaPelis/data/src/main/java/es/miempresa/data/FilmApi.kt
+++ ListaPelis/data/src/main/java/es/miempresa/data/FilmApi.kt
@@ -1,6 +1,17 @@
 package es.miempresa.data
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 const val API_KEY = "09ec48b8eb1572d60fbe4b5af38d5722"
-class FilmApi {
+interface FilmApi {
+    @GET("movie/{id}")
+    suspend fun getFilm(@Path("id")filmId:Int,
+                        @Query("language")language:String,
+                        @Query("api_key")apiKey:String = API_KEY):FilmDto
+
+
+
 
 }
