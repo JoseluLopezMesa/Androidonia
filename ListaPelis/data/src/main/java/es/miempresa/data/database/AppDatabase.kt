bc42963687540231b64726cba999d6e673a885ad
JoseluLopezMesa <joselulm9@gmail.com>
2022-02-08 17:42:40 +0100

    Actividad 12.3

---
+++ ListaPelis/data/src/main/java/es/miempresa/data/database/AppDatabase.kt
@@ -0,0 +1,9 @@
+package es.miempresa.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [FilmEntity::class], version = 1)
+abstract class AppDatabase:RoomDatabase() {
+    abstract fun filmDao():FilmDao
+}
