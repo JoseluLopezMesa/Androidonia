f31bd1a15def7976e47a1eb1f852aef8bdc914de
JoseluLopezMesa <joselulm9@gmail.com>
2022-02-14 19:58:14 +0100

    Actividad 12.5.2

--- ListaPelis/data/src/main/java/es/miempresa/data/DataModule.kt
+++ ListaPelis/data/src/main/java/es/miempresa/data/DataModule.kt
@@ -4,11 +4,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import es.miempresa.data.database.RoomModule
 import es.miempresa.domain.FilmRepository
-
-@InstallIn(SingletonComponent::class) //Con el modulo crea un paquete, los obj del repositorio
-@Module //Crea un paquete para hilt
+@Module(includes = [RoomModule::class])
+@InstallIn(SingletonComponent::class)
 abstract class DataModule {
     @Binds
-    abstract fun bindFilmRepository(impl:FilmRepositoryImplementation):FilmRepository //Toma FilmRepositoryImplementation en vez de FilmRepository
+    abstract fun bindFilmRepository(impl:FilmRepositoryImplementation):FilmRepository
 }
