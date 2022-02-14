2de54a7babd410b613bd77322e4b4ac047a3c672
JoseluLopezMesa <joselulm9@gmail.com>
2022-02-14 19:43:18 +0100

    Actividad 12.5

--- ListaPelis/data/src/main/java/es/miempresa/data/FilmRepositoryImplementation.kt
+++ ListaPelis/data/src/main/java/es/miempresa/data/FilmRepositoryImplementation.kt
@@ -1,20 +1,33 @@
 package es.miempresa.data
 
+import es.miempresa.data.database.DatabaseDataSource
 import es.miempresa.data.server.ServerDataSource
 import es.miempresa.domain.Film
 import es.miempresa.domain.FilmRepository
+import java.lang.Exception
 import javax.inject.Inject
 
 
-class FilmRepositoryImplementation @Inject constructor(private val serverDataSource: ServerDataSource):FilmRepository {
+class FilmRepositoryImplementation @Inject constructor(private val serverDataSource: ServerDataSource,
+private val databaseDataSource: DatabaseDataSource):FilmRepository {
     override suspend fun getFilm(id:Int,language:String):Film? {
-        return runCatching {
-            serverDataSource.getFilm(id,language)
-        }.getOrNull()
+        return try{
+            val filmFromServer =  serverDataSource.getFilm(id,language)
+            databaseDataSource.updateFilms(listOf(filmFromServer))
+            filmFromServer
+        }catch (exception:Exception){
+            databaseDataSource.getFilm(id)
+        }
     }
     override suspend fun getFilmList(language: String): List<Film>? {
-        return runCatching {
-            serverDataSource.getFilms(language)
-        }.getOrNull()
+        return try {
+            val filmsFromServer =  serverDataSource.getFilms(language)
+            databaseDataSource.clearFilms()
+            databaseDataSource.updateFilms(filmsFromServer)
+            filmsFromServer
+        }catch (exception: Exception){
+             databaseDataSource.getFilms()
+        }
+
     }
 }
